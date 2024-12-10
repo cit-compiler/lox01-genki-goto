283d38d3c967d68ded7229fb252974ef2c62f488
genki-goto <cige22110@g.nihon-u.ac.jp>
2024-12-10 12:42:03 +0900

    modified Scanner.java for number and string

--- codes/com/craftinginterpreters/lox/Scannar.java
+++ codes/com/craftinginterpreters/lox/Scannar.java
@@ -15,6 +15,7 @@ class Scanner {
   private int current = 0;
   private int line = 1;
 
+  //source:文章
   Scanner(String source) {
     this.source = source;
   }
@@ -29,7 +30,7 @@ class Scanner {
     tokens.add(new Token(EOF, "", null, line));
     return tokens;
   }
-  
+  //'"'まで読み進める
   private void scanToken() {
     char c = advance();
     switch (c) {
@@ -56,11 +57,78 @@ class Scanner {
       case '>':
       addToken(match('=') ? GREATER_EQUAL : GREATER);
       break;
+      case '/':
+      if (match('/')) {
+        // A comment goes until the end of the line.
+        while (peek() != '\n' && !isAtEnd()) advance();
+      } else {
+        addToken(SLASH);
+      }
+      break;
+      case ' ':
+      case '\r':
+      case '\t':
+        // Ignore whitespace.
+        break;
+
+      case '\n':
+        line++;
+        break;
+      case '"': string(); break;
 
       default:
+      if (isDigit(c)) {
+        number();
+      } else if (isAlpha(c)) {
+        identifier();
+      }else {
         Lox.error(line, "Unexpected character.");
-        break;
+      }
+      break;
+      
+      
     }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+    addToken(type);
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance(); //数字の並びがくることがわかる
+
+    // Look for a fractional part.'.'かつ後ろが数字だったら、'.'まで読み進める
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+    //NUMBERという変数に数字の並びを代入する
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private void string(){
+    while (peek() != '"' && !isAtEnd()){
+      if (peek() == '\n') line++;
+      advance();
+    }
+    //文字列おわってないのに閉じちゃったよ（文字列の閉じ忘れがあるよ）エラー
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    // The closing ".
+    advance();  //'"'がある場所を確認
+
+    // Trim the surrounding quotes.'"'を取り除いた文章を確認
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
   }
 
   private boolean match(char expected) {
@@ -69,6 +137,29 @@ class Scanner {
 
     current++;
     return true;
+  }
+  //currentを取り出す。文章終わりだったらNULLを返す
+  private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+  //current+1を取り出す
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  } 
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
   }
 
   private boolean isAtEnd() {
@@ -87,4 +178,27 @@ class Scanner {
     String text = source.substring(start, current);
     tokens.add(new Token(type, text, literal, line));
   }
+
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
 }
+
