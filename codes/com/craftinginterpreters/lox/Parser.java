fcc86469404f4dbb8de48698686cb46cad3b9370
genki-goto <cige22110@g.nihon-u.ac.jp>
2024-12-23 16:34:52 +0900

    modify Parser.java

--- codes/com/craftinginterpreters/lox/Parser.java
+++ codes/com/craftinginterpreters/lox/Parser.java
@@ -5,6 +5,7 @@ import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Parser {
+  private static class ParseError extends RuntimeException {}
   private final List<Token> tokens;
   private int current = 0;
 
@@ -102,6 +103,12 @@ class Parser {
     return false;
   }
 
+  private Token consume(TokenType type, String message) {
+    if (check(type)) return advance();
+
+    throw error(peek(), message);
+  }
+
   private boolean check(TokenType type) {
     if (isAtEnd()) return false;
     return peek().type == type;
@@ -122,6 +129,11 @@ class Parser {
 
   private Token previous() {
     return tokens.get(current - 1);
+  }
+
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
   }
 
 
