de66c1da141027ac4861265cee431cb3234bb36f
genki-goto <cige22110@g.nihon-u.ac.jp>
2025-01-20 20:24:49 +0900

    modify Parser.java,GenerateAst.java

--- codes/com/craftinginterpreters/lox/Parser.java
+++ codes/com/craftinginterpreters/lox/Parser.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.craftinginterpreters.lox.TokenType.*;
@@ -13,16 +14,58 @@ class Parser {
     this.tokens = tokens;
   }
 
-  Expr parse() {
-    try {
-      return expression();
-    } catch (ParseError error) {
-      return null;
+  List<Stmt> parse() {
+    List<Stmt> statements = new ArrayList<>();
+    while (!isAtEnd()) {
+      statements.add(declaration());
     }
+
+    return statements; 
   }
 
   private Expr expression() {
     return equality();
+  }
+
+  private Stmt declaration() {
+    try {
+      if (match(VAR)) return varDeclaration();
+
+      return statement();
+    } catch (ParseError error) {
+      synchronize();
+      return null;
+    }
+  }
+
+  private Stmt statement() {
+    if (match(PRINT)) return printStatement();
+
+    return expressionStatement();
+  }
+
+  private Stmt printStatement() {
+    Expr value = expression();
+    consume(SEMICOLON, "Expect ';' after value.");
+    return new Stmt.Print(value);
+  }
+
+  private Stmt varDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect variable name.");
+
+    Expr initializer = null;
+    if (match(EQUAL)) {
+      initializer = expression();
+    }
+
+    consume(SEMICOLON, "Expect ';' after variable declaration.");
+    return new Stmt.Var(name, initializer);
+  }
+
+  private Stmt expressionStatement() {
+    Expr expr = expression();
+    consume(SEMICOLON, "Expect ';' after expression.");
+    return new Stmt.Expression(expr);
   }
 
   private Expr equality() {
@@ -73,16 +116,6 @@ class Parser {
     return expr;
   }
 
-  private Expr unary() {
-    if (match(BANG, MINUS)) {
-      Token operator = previous();
-      Expr right = unary();
-      return new Expr.Unary(operator, right);
-    }
-
-    return primary();
-  }
-
   private Expr primary() {
     if (match(FALSE)) return new Expr.Literal(false);
     if (match(TRUE)) return new Expr.Literal(true);
@@ -90,6 +123,10 @@ class Parser {
 
     if (match(NUMBER, STRING)) {
       return new Expr.Literal(previous().literal);
+    }
+
+    if (match(IDENTIFIER)) {
+      return new Expr.Variable(previous());
     }
 
     if (match(LEFT_PAREN)) {
@@ -100,6 +137,15 @@ class Parser {
     throw error(peek(), "Expect expression.");
   }
 
+  private Expr unary() {
+    if (match(BANG, MINUS)) {
+      Token operator = previous();
+      Expr right = unary();
+      return new Expr.Unary(operator, right);
+    }
+
+    return primary();
+  }
 
   private boolean match(TokenType... types) {
     for (TokenType type : types) {
@@ -166,6 +212,5 @@ class Parser {
       advance();
     }
   }
-
 
 }
--- codes/com/craftinginterpreters/tool/GenerateAst.java
+++ codes/com/craftinginterpreters/tool/GenerateAst.java
@@ -16,7 +16,14 @@ public class GenerateAst {
       "Binary   : Expr left, Token operator, Expr right",
       "Grouping : Expr expression",
       "Literal  : Object value",
-      "Unary    : Token operator, Expr right"
+      "Unary    : Token operator, Expr right",
+      "Variable : Token name"
+    ));
+
+    defineAst(outputDir, "Stmt", Arrays.asList(
+      "Expression : Expr expression",
+      "Print      : Expr expression",
+      "Var        : Token name, Expr initializer"
     ));
   }
 
@@ -36,12 +43,12 @@ public class GenerateAst {
 
     // The AST classes.
     for (String type : types) {
-        String className = type.split(":")[0].trim();
-        String fields = type.split(":")[1].trim(); 
-        defineType(writer, baseName, className, fields);
-      }
+      String className = type.split(":")[0].trim();
+      String fields = type.split(":")[1].trim(); 
+      defineType(writer, baseName, className, fields);
+    }
 
-      // The base accept() method.
+    // The base accept() method.
     writer.println();
     writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
@@ -63,37 +70,38 @@ public class GenerateAst {
   }
 
   private static void defineType(
-    PrintWriter writer, String baseName,
-    String className, String fieldList) {
-  writer.println("  static class " + className + " extends " +
-      baseName + " {");
+      PrintWriter writer, String baseName,
+      String className, String fieldList) {
+    writer.println("  static class " + className + " extends " +
+        baseName + " {");
 
-  // Constructor.
-  writer.println("    " + className + "(" + fieldList + ") {");
+    // Constructor.
+    writer.println("    " + className + "(" + fieldList + ") {");
 
-  // Store parameters in fields.
-  String[] fields = fieldList.split(", ");
-  for (String field : fields) {
-    String name = field.split(" ")[1];
-    writer.println("      this." + name + " = " + name + ";");
+    // Store parameters in fields.
+    String[] fields = fieldList.split(", ");
+    for (String field : fields) {
+      String name = field.split(" ")[1];
+      writer.println("      this." + name + " = " + name + ";");
+    }
+
+    writer.println("    }");
+
+    // Visitor pattern.
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" +
+        className + baseName + "(this);");
+    writer.println("    }");
+    
+    // Fields.
+    writer.println();
+    for (String field : fields) {
+      writer.println("    final " + field + ";");
+    }
+
+    writer.println("  }");
   }
 
-  writer.println("    }");
-
-  // Visitor pattern.
-  writer.println();
-  writer.println("    @Override");
-  writer.println("    <R> R accept(Visitor<R> visitor) {");
-  writer.println("      return visitor.visit" +
-      className + baseName + "(this);");
-  writer.println("    }");
-
-  // Fields.
-  writer.println();
-  for (String field : fields) {
-    writer.println("    final " + field + ";");
-  }
-
-  writer.println("  }");
-}
 }
